aef6356dbac446e5c8815af74e745e174a318cb5
Angel <aleoreina@gmail.com>
2020-04-08 19:55:16 -0400

    Fixes issues mkvirtualenv cmd not found + Returning permissions

--- install-djmserver.sh
+++ install-djmserver.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #Upgrade Linux
 sudo apt-get update && sudo apt-get upgrade
 
@@ -26,27 +28,30 @@ sudo apt-get install gcc g++ make -y
 sudo apt install nodejs -y
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install yarn -y
 #npm install -g yarn
-sudo -H pip3 install --upgrade pip
+sudo -H pip3 install --upgrade pip 
 sudo -H pip3 install virtualenv virtualenvwrapper
 
 # VirtualEnvWrapper setting up (for user django)
 su django -c "echo 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3' >> ~/.bashrc"
-su django -c "echo 'export WORKON_HOME=~/Env' >> ~/.bashrc"
+su django -c "echo 'export WORKON_HOME=/home/django/Env' >> ~/.bashrc"
 su django -c "echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc"
-su django -c "source ~/.bashrc &&"
 
 
 # Getting up a clean project of django
 cd /home/django
 su django -c "git clone https://github.com/aleoreina/django-server-manager.git"
-su django -c "source ~/.bashrc && mv ./django-server-manager ./manager"
+su django -c "mv ./django-server-manager ./manager"
 cd /home/django/manager
-su django -c "source ~/.bashrc && mkvirtualenv manager"
-su django -c "source ~/.bashrc && pip install -r req.txt"
+source /home/django/.bashrc
+mkvirtualenv manager
+pip install -r req.txt
 su django -c "yarn"
-su django -c "source ~/.bashrc && ./manage.py collecstatic"
+./manage.py collecstatic
+
+# Returning permission for enviroment to user django
+chown -R django /home/django/Env
 
 # Installing UWSGI
 sudo pip3 install uwsgi
